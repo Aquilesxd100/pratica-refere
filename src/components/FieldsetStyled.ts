f452d829c16fac26b4365844677d125eba20d001
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-08-09 19:29:28 -0300

    style: cria FieldsetStyled

---
+++ src/components/FieldsetStyled.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const FieldsetStyled = styled.fieldset`
+    padding: 10px;
+    width: 98%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    div {
+        display: block;
+        width: 100%;
+        background-color: gray;
+        margin: 2px;
+    }
+`;
+
+export default FieldsetStyled;
