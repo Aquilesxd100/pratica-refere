d9a894aac9094e14a6a978be99de6d46962add48
Aquilesxd <fernandoalanfillmann@gmail.com>
2023-08-10 10:33:29 -0300

    feat: add rotas para user1 & user2

--- src/config/navigation/index.ts
+++ src/config/navigation/index.ts
@@ -7,7 +7,9 @@ const navigation: NavigationType[] = [
   { url: '/', label: 'Seguro de Automóvel' },
   { url: '/seguroDePessoa', label: 'Seguro de Pessoas' },
   { url: '/seguroDeCasa', label: 'Seguro de Residência' },
-  { url: '/contratos', label: 'Contratos Feitos' }
+  { url: '/contratos', label: 'Contratos Feitos' },
+  { url: '/user', label: 'User1' },
+  { url: '/user2', label: 'User2' }
 ];
 
 export default navigation;
